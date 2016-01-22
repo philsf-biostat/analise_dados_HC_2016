4d1960db3e9ee39da3cf6f9c08ff6ffa3266e298
Felipe Figueiredo <philsf79@gmail.com>
2016-01-22 05:53:50 -0200

    Separar o dataset em numéricos e categóricos.

---
+++ scripts/input.R
@@ -0,0 +1,12 @@
+## Carregando o dataset ####
+cobra <- read.table("../dataset/hc-dataset.dat")
+
+## Dados numéricos ####
+cobra_num <- cobra[,c("Idade","IMC","Albumina","Globulina","Tempo")]
+write.table(cobra_num,file = "../dataset/dadosnum.dat")
+
+## Dados categóricos ####
+cobra_fat <-cobra[,c("ATB","Sexo","Diagnóstico","DM","ASA","Desfecho")]
+write.table(cobra_fat,file = "../dataset/dadosfat.dat")
+
+rm(cobra,cobra_fat,cobra_num)
