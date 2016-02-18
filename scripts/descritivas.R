0d8bfaf470c9123d127d2b54bf912080e8f755b7
Felipe Figueiredo <philsf79@gmail.com>
2016-02-18 17:25:14 -0200

    Caminhos alterados
    Os scripts agora serão feitos levando como base o diretório do projeto,
    e não o diretório scripts/.

--- scripts/descritivas.R
+++ scripts/descritivas.R
@@ -1,8 +1,9 @@
 ## Dados numéricos ####
-dados_num <- read.table("../dataset/dadosnum.dat")
+dados_num <- read.table("dataset/dadosnum.dat")
+
 desc_fun <- function(x) c(mean(x,na.rm = T),sd(x, na.rm = T),median(x, na.rm = T),IQR(x, na.rm = T))
 desc_num <- vapply(dados_num,desc_fun,c("Média" = 0, "DP" = 0, "Mediana" = 0, "AIQ" = 0) )
 desc_num <- t(round(desc_num,digits = 2 ))
 
-write.table(desc_num,file = "../resultados/descritivas_num.dat")
+write.table(desc_num,file = "resultados/descritivas_num.dat")
 rm(dados_num,desc_fun,desc_num)
