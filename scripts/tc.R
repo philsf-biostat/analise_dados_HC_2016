d807f3a925a8330863f6210d27982a5416c4983e
Felipe Figueiredo <philsf79@gmail.com>
2016-04-26 13:11:08 -0300

    formatação dos comentários
    Para facilitar a exportação para MD

--- scripts/tc.R
+++ scripts/tc.R
@@ -5,34 +5,34 @@ dados_fat <- read.table("dataset/dadosfat.dat")
 attach(dados_num)
 attach(dados_fat)
 
-## Tabelas de contingencia ####
+# Tabelas de contingencia ####
 
-# Desfecho por sexo
+## Desfecho por sexo
 sexo_desfecho <- table(Desfecho, Sexo)
 t(sexo_desfecho)
 t(round(prop.table(sexo_desfecho)*100, digits = 1))
 
-# Desfecho por ASA
+## Desfecho por ASA
 asa_desfecho <- table(Desfecho, ASA)
 t(asa_desfecho)
 t(round(prop.table(asa_desfecho)*100, digits = 1))
 
-# Desfecho por diabete
+## Desfecho por diabete
 dm_desfecho <- table(Desfecho, DM)
 t(dm_desfecho)
 t(round(prop.table(dm_desfecho)*100, digits = 1))
 
-# Desfecho por diagnostico
+## Desfecho por diagnostico
 diag_desfecho <- table(Desfecho, Diagnóstico)
 t(diag_desfecho)
 t(round(prop.table(diag_desfecho)*100, digits = 1))
 
-# Desfecho por tratamento
+## Desfecho por tratamento
 atb_desfecho <- table(Desfecho, ATB)
 t(atb_desfecho)
 t(round(prop.table(atb_desfecho)*100, digits = 1))
 
-# ASA por tratamento
+## ASA por tratamento
 asa_atb <- table(ATB, ASA)
 t(asa_atb)
 t(round(prop.table(asa_atb)*100, digits = 1))
