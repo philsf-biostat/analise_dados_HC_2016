15e15026e76307d4760dedcfe0b50ec5dee10c37
Felipe Figueiredo <philsf79@gmail.com>
2016-04-26 13:01:17 -0300

    Testes movidos para script proprio
    Os testes de independência das tabelas de contingência foram movidos
    para o script testes.R, que concentrará todas as análises de
    significância. Agora o script tc.R apenas gera as tabelas de
    contingência, mantendo as variáveis mais organizadas, e facilitando a
    inclusão das análises no relatório.

--- scripts/tc.R
+++ scripts/tc.R
@@ -10,38 +10,26 @@ attach(dados_fat)
 # Desfecho por sexo
 sexo_desfecho <- table(Desfecho, Sexo)
 t(round(prop.table(sexo_desfecho)*100, digits = 1))
-fisher.test(sexo_desfecho)
-# chisq.test(sexo_desfecho)
 
 # Desfecho por ASA
 asa_desfecho <- table(Desfecho, ASA)
 t(round(prop.table(asa_desfecho)*100, digits = 1))
-fisher.test(asa_desfecho)
-# chisq.test(asa_desfecho)
 
 # Desfecho por diabete
 dm_desfecho <- table(Desfecho, DM)
 t(round(prop.table(dm_desfecho)*100, digits = 1))
-fisher.test(dm_desfecho)
-# chisq.test(dm_desfecho)
 
 # Desfecho por diagnostico
 diag_desfecho <- table(Desfecho, Diagnóstico)
 t(round(prop.table(diag_desfecho)*100, digits = 1))
-fisher.test(diag_desfecho)
-# chisq.test(diag_desfecho)
 
 # Desfecho por tratamento
 atb_desfecho <- table(Desfecho, ATB)
 t(round(prop.table(atb_desfecho)*100, digits = 1))
-fisher.test(atb_desfecho)
-# chisq.test(atb_desfecho)
 
 # ASA por tratamento
 asa_atb <- table(ATB, ASA)
 t(round(prop.table(asa_atb)*100, digits = 1))
-fisher.test(asa_atb)
-# chisq.test(asa_atb)
 
 ## limpar dados ####
 detach(dados_num)
