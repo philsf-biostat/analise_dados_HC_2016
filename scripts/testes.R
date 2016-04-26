127ccb269ae9a182af4063424fe560f9f9cf3c73
Felipe Figueiredo <philsf79@gmail.com>
2016-04-26 14:51:20 -0300

    categoricas formatadas
    testes de independência das variáveis categóricas foram formatados e
    exportados (manualmente) para MD.

--- scripts/testes.R
+++ scripts/testes.R
@@ -57,16 +57,44 @@ wilcox.test(Tempo ~ ATB)
 ## Proteína total por tratamento
 wilcox.test(ptn ~ ATB)
 
-## Categóricas por desfecho
+## Categóricas por desfecho (Fischer)####
+
+# Testes de independência
+
+## Desfecho por sexo
 fisher.test(sexo_desfecho)
-# chisq.test(sexo_desfecho)
+
+## Desfecho por ASA
 fisher.test(asa_desfecho)
-# chisq.test(asa_desfecho)
+
+## Desfecho por diabete
 fisher.test(dm_desfecho)
-# chisq.test(dm_desfecho)
+
+## Desfecho por diagnóstico
 fisher.test(diag_desfecho)
-# chisq.test(diag_desfecho)
+
+## Desfecho por tratamento
 fisher.test(atb_desfecho)
-# chisq.test(atb_desfecho)
+
+## ASA por tratamento
 fisher.test(asa_atb)
+
+## Categóricas por desfecho (qui-quadrado)####
+
+## Desfecho por sexo
+# chisq.test(sexo_desfecho)
+
+## Desfecho por ASA
+# chisq.test(asa_desfecho)
+
+## Desfecho por diabete
+# chisq.test(dm_desfecho)
+
+## Desfecho por diagnóstico
+# chisq.test(diag_desfecho)
+
+## Desfecho por tratamento
+# chisq.test(atb_desfecho)
+
+## ASA por tratamento
 # chisq.test(asa_atb)
