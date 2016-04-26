c805074344fb3a0fc8eb79413e9dc21133655444
Felipe Figueiredo <philsf79@gmail.com>
2016-04-26 13:10:22 -0300

    exportar tabelas
    Frequencias agora são geradas, e exportadas (manualmente) para TXT e para MD.

--- scripts/tc.R
+++ scripts/tc.R
@@ -9,26 +9,32 @@ attach(dados_fat)
 
 # Desfecho por sexo
 sexo_desfecho <- table(Desfecho, Sexo)
+t(sexo_desfecho)
 t(round(prop.table(sexo_desfecho)*100, digits = 1))
 
 # Desfecho por ASA
 asa_desfecho <- table(Desfecho, ASA)
+t(asa_desfecho)
 t(round(prop.table(asa_desfecho)*100, digits = 1))
 
 # Desfecho por diabete
 dm_desfecho <- table(Desfecho, DM)
+t(dm_desfecho)
 t(round(prop.table(dm_desfecho)*100, digits = 1))
 
 # Desfecho por diagnostico
 diag_desfecho <- table(Desfecho, Diagnóstico)
+t(diag_desfecho)
 t(round(prop.table(diag_desfecho)*100, digits = 1))
 
 # Desfecho por tratamento
 atb_desfecho <- table(Desfecho, ATB)
+t(atb_desfecho)
 t(round(prop.table(atb_desfecho)*100, digits = 1))
 
 # ASA por tratamento
 asa_atb <- table(ATB, ASA)
+t(asa_atb)
 t(round(prop.table(asa_atb)*100, digits = 1))
 
 ## limpar dados ####
