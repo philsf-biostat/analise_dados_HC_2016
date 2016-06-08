aa33ff34620fc3fa9db053595ee93ae5c936db75
Felipe Figueiredo <philsf79@gmail.com>
2016-06-08 17:03:01 -0300

    prontuário [anonimo] removido
    O prontuário de número [omitido] foi removido do dataset. Todos os
    resultados deverão ser gerados novamente a partir daqui.

--- scripts/input-raw.R
+++ scripts/input-raw.R
@@ -1,12 +1,15 @@
 library(xlsx)
-dados<-read.xlsx("../../Profilaxia ATB_Amanda4.xlsx",1)
+dados<-read.xlsx("../Profilaxia ATB_Amanda4.xlsx",1)
+
+pront_remover <- 0
+dados <- dados[dados$Prontuario != pront_remover,]
 
 ## Anonimizando os dados, e selecionando apenas as colunas relevantes
 cobra <- dados[,c("ATB","Idade","Sexo","IMC","Diagnóstico","DM","Albumina","Globulina","ASA","Tempo","Desfecho")]
 str(cobra)
 summary(cobra)
 
-write.table(cobra,file="../dataset/hc-dataset.dat")
+write.table(cobra,file="dataset/hc-dataset.dat")
 
 summary(modelo <- glm(Desfecho ~ ATB, family = binomial(link = "logit"), data=cobra ))
 
